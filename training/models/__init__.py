013e13bc93d7261b1e2b52afa0a889418b3fa1dc
RedAlexDad <boss6852@gmail.com>
2026-04-21 20:53:51 +0300

    refactor: заменить MobileNetV2 на ResNet20
    Добавлена модель ResNet20, сохранена также MobileNetV2

--- training/models/__init__.py
+++ training/models/__init__.py
@@ -1,5 +1,6 @@
 from .mlp import MLPModel
 from .cnn import CNNModel
 from .resnet20 import ResNet20Model
+from .mobilenet import MobileNetModel
 
-__all__ = ['MLPModel', 'CNNModel', 'ResNet20Model']
+__all__ = ['MLPModel', 'CNNModel', 'ResNet20Model', 'MobileNetModel']
